c9ffaa077626ec67b9b777c2d21a05a0e5036566
onemanzerg <donnydeus@gmail.com>
2020-09-22 19:52:21 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+def some():
+    print("some")
+
+
+def bitcoin_to_usd(btc, curs):
+    amount = btc * curs
+    print(amount)
+
+
+# bitcoin_to_usd(round(4.4), 4100)
---
+++ homework_while_for.py
@@ -0,0 +1,15 @@
+
+
+
+for n in range(50):
+    if n % 2 == 0:
+        print(n)
+
+
+
+
+
+# d = 2
+# while d < 50:
+#     print(d)
+#     d = d + 2
