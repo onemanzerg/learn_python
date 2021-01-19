03ff90f5ca27497b8094087b82d119eab8729174
onemanzerg <donnydeus@gmail.com>
2021-01-19 23:22:54 +0300

    obnova

--- parser/main.py
+++ parser/main.py
@@ -1,14 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://minfin.com.ua/currency/nbu/'
+url = 'https://www.banki.ru/products/currency/cash/ivanovo/'
 
 sourse = requests.get(url)
 mainText = sourse.text
 soup = BeautifulSoup(mainText)
 
-table = soup.find('table', {'class':'table-auto'})
-tr = table.find('td', {'class':'responsive-hide'})
-tr = tr.text
+table = soup.find('table', {'class':'currency-table__table'})
+usd = table.find('div', {'class':'currency-table__large-text'})
+usd = usd.text
 
-print(tr)
+#eu = table.find('div', {'class':'currency-table__large-text'})
+#eu = eu.text
+
+#for tag in soup.findAll("currency-table__large-text"):
+#        print("{0}: {1}".format(tag.name, tag.text))
+
+print(usd)
